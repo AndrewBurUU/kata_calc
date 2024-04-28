c6144eb8bd8afaa27dbc1bf5fbd796a49d3dc652
AndrewBurUU <andriv80@gmail.com>
2024-04-28 18:09:57 +0800

    gitignore setting

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static String calc(String input) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
